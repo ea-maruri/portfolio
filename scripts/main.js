7b9f66d9d83e2de8fd887e3e0a0db51e83fe885f
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-07-01 16:01:24 -0500

    Correcting aside and index

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,7 @@ const social_chip_title = document.getElementById("social-chip-title");
 const available_width = window.screen.availWidth;
 console.log("Available width:" + window.screen.availWidth);
 
+
 // Event listener with arrow notation
 social_chip.addEventListener("click", () => {
   // Ensuring if elements are in
@@ -25,24 +26,26 @@ social_chip.addEventListener("click", () => {
       social_chip.style.marginLeft = "0";
 
       social_chip_title.style.fontSize = ".7rem";
-      social_chip_title.style.left = ".2rem";
+      social_chip_title.style.left = ".25rem";
       social_chip_title.style.top = ".3rem";
 
       if (available_width <= 650) {
         social_chip.style.width = "15vw";
         social_chip.style.height = "15vw";
-      } else {
+      } 
+      else {
         social_chip.style.width = "10vh";
         social_chip.style.height = "10vh";
       }
-    } else {
+    } 
+    else {
       social_card.style.display = "none";
 
       social_chip.style.marginLeft = "-2.3rem";
 
       social_chip_title.style.display = "block";
       social_chip_title.style.fontSize = "1rem";
-      social_chip_title.style.left = ".9rem";
+      social_chip_title.style.left = ".75rem";
 
       if (available_width <= 650) {
         social_chip.style.width = "15vw";
@@ -50,7 +53,8 @@ social_chip.addEventListener("click", () => {
         social_chip.style.marginLeft = "-1.9rem";
 
         social_chip_title.style.fontSize = ".7rem";
-      } else {
+      } 
+      else {
         social_chip.style.width = "16.66vh";
         social_chip.style.height = "16.66vh";
         social_chip_title.style.fontSize = "1rem";
