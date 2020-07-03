03c3fc1a79a847ed1ac6c9b749632b35f4478a61
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-07-03 15:10:12 -0500

    Change arrow. It reacts to scroll

--- scripts/main.js
+++ scripts/main.js
@@ -8,19 +8,34 @@ copyright.innerHTML = "&#169; Copyright " + year;
 
 
 // Check Scroll
-//let documentHeight = $(window).height(); 
-//const $document = $(document);
+let arrowContainer = document.getElementById("marrow-container");
+let arrow = document.getElementById("marrow");
 
+let body = document.body;
+let html = document.documentElement;
 
+const height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
 
-//let arrow
-var countdown = setInterval(function() {
+console.log("Height: " + height + "\nMiddle: " + height/2);
+
+// Each half second
+const counting = setInterval(function() {
     let scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
 
     console.log("Scroll: "+ scrollTop);
 
-    if(scrollTop){
-
+    if(scrollTop >= height/2){
+        console.log("Turn around arrow");
+        
+        arrowContainer.style.flexDirection = "column-reverse"; 
+        arrow.className = "arrow up";
+        arrow.style.marginBottom = "-20px";
+    }
+    else{
+        console.log("Return arrow")
+        arrowContainer.style.flexDirection = "column";
+        arrow.className = "arrow down";
     }
 
-}, 2000);
+}, 500);
