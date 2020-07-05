0e93b1da98322cdc9ee1ee6f0c2164bd05a2c91e
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-07-05 15:05:21 -0500

    add functionallity to forms

--- scripts/main.js
+++ scripts/main.js
@@ -72,5 +72,82 @@ function document_goto(event){
 
 
 
-// Contact Form
+// Contact Form Modal
+let formMail = document.getElementById("modal_txt_email");
+let formName = document.getElementById("modal_txt_name");
+let formTopic = document.getElementById("modal_txt_topic");
+let formRequest = document.getElementById("modal_txt_request");
+let successMessageModal = document.getElementById("success-form-message-modal");
 
+let btnSend = document.getElementById("modal_btn_send");
+
+btnSend.addEventListener("click", mSubmitModalForm);
+
+function mSubmitModalForm(event){
+    let valueMail = formMail.value;
+    let valueName = formName.value;
+    let valueTopic = formTopic.value;
+    let valueRequest = formRequest.value;
+
+    console.log("Received "+ valueMail + valueName);
+
+    if(valueMail && valueName && valueTopic && valueRequest){
+        console.log("Send this");
+        console.log("Mail: " + valueMail);
+        console.log("Name: " + valueName);
+        console.log("Topic: " + valueTopic);
+        console.log("Request: " + valueRequest);
+
+        successMessageModal.innerHTML = "Excelent, your request has been sent successfully!";
+
+        formMail.value = "";
+        formName.value = "";
+        formTopic.value = "";
+        formRequest.value = "";
+    }
+    else{
+        alert("Please, do not leave blanks in the form");
+    }
+}
+
+
+// Contact Form About page
+let aboutFormMail = document.getElementById("about_txt_email");
+let aboutFormName = document.getElementById("about_txt_name");
+let aboutFormTopic = document.getElementById("about_txt_topic");
+let aboutFormRequest = document.getElementById("about_txt_request");
+let successMessage = document.getElementById("success-form-message");
+
+let aboutbtnSend = document.getElementById("about_btn_send");
+
+if(aboutbtnSend){
+    aboutbtnSend.addEventListener("click", mSubmitAboutForm);
+}
+
+function mSubmitAboutForm(event){
+    console.log("Something")
+    let valueMail = aboutFormMail.value;
+    let valueName = aboutFormName.value;
+    let valueTopic = aboutFormTopic.value;
+    let valueRequest = aboutFormRequest.value;
+
+    console.log("Received "+ valueMail + valueName);
+
+    if(valueMail && valueName && valueTopic && valueRequest){
+        console.log("Send this");
+        console.log("Mail: " + valueMail);
+        console.log("Name: " + valueName);
+        console.log("Topic: " + valueTopic);
+        console.log("Request: " + valueRequest);
+
+        successMessage.innerHTML = "Excelent, your request has been sent successfully!";
+
+        aboutFormMail.value = "";
+        aboutFormName.value = "";
+        aboutFormTopic.value = "";
+        aboutFormRequest.value = "";
+    }
+    else{
+        alert("Please, do not leave blanks in the form");
+    }
+}
